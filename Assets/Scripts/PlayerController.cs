a2b877d2aabc4568494cffaf7e6e9bc31199b235
aliarii <34171708+aliarii@users.noreply.github.com>
2021-12-21 22:03:21 +0300

    Optimizations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float speed = 20.0f;
-    [SerializeField] float turnSpeed = 45.0f;
+    //[SerializeField] float speed = 20.0f;
+    [SerializeField] private float horsePower;
+    [SerializeField] private float turnSpeed = 45.0f;
     private float horizontalInput;
-    private float forwardInput;
+    private float verticalInput;
+    private Rigidbody playerRb;
+    [SerializeField] GameObject centerOfMasss;
 
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMasss.transform.position;
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
 
         horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        verticalInput = Input.GetAxis("Vertical");
         // Moves the car forward based on vertical input
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        playerRb.AddRelativeForce(Vector3.forward * horsePower * verticalInput);
         // Rotates the car based on horizontal input
         transform.Rotate(Vector3.up, horizontalInput * Time.deltaTime * turnSpeed);
         //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
