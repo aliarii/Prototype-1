7c920bb91c593d7f75c796bdec431b13de2909a7
aliarii <34171708+aliarii@users.noreply.github.com>
2021-12-21 19:39:59 +0300

    Optimization

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -6,7 +6,7 @@ public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
 
-    private Vector3 offset = new Vector3(0, 5, -7);
+    [SerializeField] Vector3 offset = new Vector3(0, 5, -7);
 
     // Update is called once per frame
     void LateUpdate()
